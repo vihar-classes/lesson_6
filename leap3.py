a685d5afde758332c27523852c228957b168cea3
Vihar <vihar_classes@proton.me>
2025-12-04 19:43:49 +0800

    Create leap3.py

---
+++ leap3.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open("https://www.google.com/search?q=is+this+year+a+leap+year&rlz=1C1GCEA_enAU1183AU1183&oq=is+this+year+a+leap+year&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIICAcQABgWGB4yCAgIEAAYFhgeMggICRAAGBYYHtIBCDYyMTRqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8")
